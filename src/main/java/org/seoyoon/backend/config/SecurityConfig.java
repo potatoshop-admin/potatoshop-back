a5150f1941491e3bb06af83e2389c1e5b31b60f2
sychoi <cinnamon1995@naver.com>
2025-11-25 17:33:17 +0900

    feat: add global config and common response dto

---
+++ src/main/java/org/seoyoon/backend/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package org.seoyoon.backend.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    //Bean -> 스프링이 뽑는 오브젝트
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//filterChain => 모든 유저의 요청과 서버의 응답 사이에 자동으로 실행해주고 싶은 코드 담는 곳
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable());
+
+        http.cors(Customizer.withDefaults());
+
+        http.formLogin((auth) -> auth.disable());
+
+        http.httpBasic((auth) -> auth.disable());
+
+        http.authorizeHttpRequests((authorize) ->
+                authorize
+                        .requestMatchers("/auth/login",  "/adminUser","/stores" ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/itemsImage/**").authenticated()
+                        .anyRequest().authenticated()
+//                authorize.requestMatchers("/**").permitAll()
+        );
+
+        http.exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            // 인증 자체가 없을 때 → 401
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            // 인증은 됐지만 권한이 부족할 때 → 403
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+                        })
+
+        );
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // 세션 설정
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+}
